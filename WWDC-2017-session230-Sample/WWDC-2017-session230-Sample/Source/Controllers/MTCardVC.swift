c6c708f520628400c6f3a444166e080325005bd5
engili <lmt.coding@gmail.com>
2018-04-23 20:01:09 +0800

    1.add MTPhotoVC MTCardVC 2. add Navaigation

---
+++ WWDC-2017-session230-Sample/WWDC-2017-session230-Sample/Source/Controllers/MTCardVC.swift
@@ -0,0 +1,35 @@
+//
+//  MTCardVC.swift
+//  WWDC-2017-session230-Sample
+//
+//  Created by LiMengtian on 2018/4/23.
+//  Copyright © 2018 LiMengtian. All rights reserved.
+//
+
+import UIKit
+
+class MTCardVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
