6dc6f337297dee7039575b2dfcfce90befb146e4
engili <lmt.coding@gmail.com>
2018-04-25 21:01:10 +0800

    add pangesture animation

--- WWDC-2017-session230-Sample/WWDC-2017-session230-Sample/Source/Views/MTCardView.swift
+++ WWDC-2017-session230-Sample/WWDC-2017-session230-Sample/Source/Views/MTCardView.swift
@@ -25,18 +25,21 @@ class MTCardView: UIView {
         
         fileprivate static let kCollapsedFrame = CGRect(x: 0, y: kCollapsedY, width: kViewWith, height: kViewHeight)
         fileprivate static let kExpandedFrame = CGRect(x: 0, y: kExpandedY, width: kViewWith, height: kViewHeight)
+        
+        fileprivate static let duration: TimeInterval = 0.75
     }
     
     fileprivate var topTitleView: UIView!
     fileprivate var titleLabel: UILabel!
-    
     fileprivate var runningAnimators = [UIViewPropertyAnimator]()
-    
     fileprivate var currentState: State
-    
+    fileprivate var aimState: State?
     fileprivate var frameAnimatorObsverToken: NSKeyValueObservation?
     
     //MARK: - Life Cycle
+    deinit {
+        print("MTCardView deinit")
+    }
     
     init(state: State) {
         var frame:CGRect
@@ -48,6 +51,8 @@ class MTCardView: UIView {
         }
         currentState = state
         super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.white
         
         self.setupSubviews()
         self.setupConstraints()
@@ -98,7 +103,7 @@ class MTCardView: UIView {
         topTitleView.addSubview(titleLabel)
     }
     
-    // Gesture
+    //MARK: - Gesture
     fileprivate func addGestureRecognizer() {
         topTitleView.isUserInteractionEnabled = true
         
@@ -110,21 +115,31 @@ class MTCardView: UIView {
     }
     
     @objc fileprivate func handleTapGesture(_ gesture: UITapGestureRecognizer) {
-        switch currentState {
-        case .Collapsed:
-            self.animateOrReverseRunningTransition(state: .Expanded, duration: 1.0)
-            currentState = .Expanded
-        case .Expanded:
-            self.animateOrReverseRunningTransition(state: .Collapsed, duration: 1.0)
-            currentState = .Collapsed
-        }
+        aimState = currentState == .Collapsed ? .Expanded : .Collapsed
+        self.animateOrReverseRunningTransition(state: aimState!, duration: Constants.duration)
     }
     
     @objc fileprivate func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
-        
+    
+        switch gesture.state {
+        case .began:
+            aimState = currentState == .Collapsed ? .Expanded : .Collapsed
+            self.startInteractiveTransition(state: aimState!, duration: Constants.duration)
+        case .changed:
+            let transtion = gesture.translation(in: self)
+            let deltY = Constants.kExpandedY - Constants.kCollapsedY
+            self.updateInteractiveTransition(fractionComplete: fabs(transtion.y / deltY))
+        case .ended:
+            self.continueInteractiveTransition(cancel: false)
+            break
+        default:
+            break
+        }
+
     }
     
-    func animateTransitionIfNeeded(state:State, duration: TimeInterval) {
+    //MARK: - Animation
+    fileprivate func animateTransitionIfNeeded(state:State, duration: TimeInterval) {
         if runningAnimators.isEmpty {
             let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1.0) {
                 switch state {
@@ -137,27 +152,35 @@ class MTCardView: UIView {
                 }
             }
             
-            frameAnimator.startAnimation()
             frameAnimator.pausesOnCompletion = true
             runningAnimators.append(frameAnimator)
 
             frameAnimatorObsverToken = frameAnimator.observe(\.isRunning) { [unowned self] (animator, change) in
                 if !animator.isRunning {
-                    switch self.currentState {
-                    case .Collapsed:
-                        self.frame = Constants.kCollapsedFrame
-                    case .Expanded:
-                        self.frame = Constants.kExpandedFrame
+                    if let aimState = self.aimState {
+                        self.currentState = aimState
+                        switch aimState {
+                        case .Collapsed:
+                            self.frame = Constants.kCollapsedFrame
+                        case .Expanded:
+                            self.frame = Constants.kExpandedFrame
+                        }
                     }
+                    self.runningAnimators.removeAll()
+                    self.frameAnimatorObsverToken = nil
+                    self.aimState = nil
                 }
             }
     
         }
     }
     
-    func animateOrReverseRunningTransition(state: State, duration: TimeInterval) {
+    fileprivate func animateOrReverseRunningTransition(state: State, duration: TimeInterval) {
         if runningAnimators.isEmpty {
             self.animateTransitionIfNeeded(state: state, duration: duration)
+            for animator in runningAnimators {
+                animator.startAnimation()
+            }
         } else {
             for animator in runningAnimators {
                 animator.isReversed = !animator.isReversed
@@ -165,4 +188,37 @@ class MTCardView: UIView {
             }
         }
     }
+    
+    fileprivate func startInteractiveTransition(state: State, duration: TimeInterval) {
+        if runningAnimators.isEmpty {
+            self.animateTransitionIfNeeded(state: state, duration: duration)
+            for animator in runningAnimators {
+                animator.pauseAnimation()
+            }
+        } else {
+            for animator in runningAnimators {
+                animator.isReversed = !animator.isReversed
+                animator.pauseAnimation()
+            }
+        }
+    }
+    
+    fileprivate func updateInteractiveTransition(fractionComplete: CGFloat) {
+        for animator in runningAnimators {
+            animator.fractionComplete = fractionComplete
+        }
+    }
+    
+    fileprivate func continueInteractiveTransition(cancel: Bool) {
+        
+        if cancel {
+            
+        } else {
+            for animtor in runningAnimators {
+                animtor.continueAnimation(withTimingParameters: nil, durationFactor: 1.0)
+            }
+        }
+        
+    }
+    
 }
